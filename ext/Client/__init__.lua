538faa7e1ba2ec41180b82422c2a4b9d90bfdf18
FlashHit <56718716+FlashHit@users.noreply.github.com>
2020-12-13 21:33:26 +0100

    implement the chat lag fix

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -1181,4 +1181,12 @@ function BetterIngameAdmin:OnApplyMapSettings(args)
 end
 -- Endregion
 
+-- Region Fix Chatlag
+ResourceManager:RegisterInstanceLoadHandler(Guid('3E6AF1E2-B10E-11DF-9395-96FA88A245BF'), Guid('78B3E33E-098B-3320-ED15-89A36F04007B'), function(instance)
+	instance = UIMessageCompData(instance)
+	instance:MakeWritable()
+	MessageInfo(instance.chatMessageInfo).messageQueueSize = 6
+end)
+--Endregion
+
 g_BetterIngameAdmin = BetterIngameAdmin()
