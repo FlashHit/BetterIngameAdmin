0bb987d8b24ff8c46068d57d757aefea3256ad71
FlashHit <56718716+FlashHit@users.noreply.github.com>
2020-12-30 03:04:30 +0100

    increase MessageQueueSize from 6 to 20
    so the chat can keep up if some admins spam it 10 times a second.

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -1215,7 +1215,7 @@ end
 ResourceManager:RegisterInstanceLoadHandler(Guid('3E6AF1E2-B10E-11DF-9395-96FA88A245BF'), Guid('78B3E33E-098B-3320-ED15-89A36F04007B'), function(instance)
 	instance = UIMessageCompData(instance)
 	instance:MakeWritable()
-	MessageInfo(instance.chatMessageInfo).messageQueueSize = 6
+	MessageInfo(instance.chatMessageInfo).messageQueueSize = 20
 end)
 --Endregion
 
