74f792d108d22f56f4aa711bc38c41ac02ebc137
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-01-02 23:49:44 +0100

    moved to Server folder

--- ext/Shared/__init__.lua
+++
@@ -1,9 +0,0 @@
-Events:Subscribe('Level:LoadingInfo', function(screenInfo)
-    if screenInfo == "Running" or screenInfo == "Blocking on shader creation" then
-		local syncedBFSettings = ResourceManager:GetSettings("SyncedBFSettings")
-		if syncedBFSettings ~= nil then
-			syncedBFSettings = SyncedBFSettings(syncedBFSettings)
-			syncedBFSettings.teamSwitchingAllowed = false
-		end
-	end
-end)
